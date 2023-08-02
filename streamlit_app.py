0b3f4b7cfd2ed397c83732c8f4e40896b39d2296
Simon Liang <siliang@deloitte.co.nz>
2023-08-03 00:21:31 +1200

    add requests of fruityvice api

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import requests
 
 st.title("My Parents New Healthy Diner")
 st.header("Breakfast Favorites")
@@ -18,3 +19,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 st.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
