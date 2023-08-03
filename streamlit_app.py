1bddefffe62cb89973fc1f343db79e3583726c84
Simon Liang <siliang@deloitte.co.nz>
2023-08-03 22:19:43 +1200

    change streamlit interface to show added fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,4 @@ st.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit to the list
 add_my_fruit = st.text_input("What fruit would you like to add?", "jackfruit")
-st.text("Thanks for adding " + add_my_fruit)
+st.write("Thanks for adding ", add_my_fruit)
