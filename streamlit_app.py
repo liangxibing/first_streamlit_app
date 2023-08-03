f6e82946fa62b753cb9d483265ed22ddc4a8e246
Simon Liang <siliang@deloitte.co.nz>
2023-08-03 22:51:31 +1200

    fix typo of variable to insert into fruit list table

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ if st.button("Get Fruit Load List"):
 # Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into pc_rivery_db.public.fruit_load_list (fruit_name) values ('" + add_my_fruit + "')")
+    my_cur.execute("insert into pc_rivery_db.public.fruit_load_list (fruit_name) values ('" + new_fruit + "')")
     return "Thanks for adding " + new_fruit
 
 add_my_fruit = st.text_input("What fruit would you like to add?")
