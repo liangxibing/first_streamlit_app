2ceb8257573cc19b227f3582fa486a9ea56155e5
Simon Liang <siliang@deloitte.co.nz>
2023-08-03 22:54:00 +1200

    add cursor close and change a bit header label

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ try:
 except URLError as e:
   st.error()
 
-st.header("The fruit load list from contains:")
+st.header("View Our Fruit List - Add Your Favorites!")
 # Snowflake-related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
@@ -48,6 +48,7 @@ def get_fruit_load_list():
 if st.button("Get Fruit Load List"):
   my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   st.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit to the list
@@ -60,4 +61,5 @@ add_my_fruit = st.text_input("What fruit would you like to add?")
 if st.button("Add a Fruit to the List"):
   my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
   st.text(insert_row_snowflake(add_my_fruit))
+  my_cnx.close()
 
